94623d2c02bd8a99a2b06668287d562fe92b8839
afix88 <123927896+afix88@users.noreply.github.com>
2023-10-14 14:26:46 +0700

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -20,7 +20,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\afixm\OneDrive\05. Kumpulan Dataset")
+    os.chdir(r"https://github.com/afix88/streamlit_tes/blob/main/")
     df = pd.read_csv("Sample - Superstore.csv", encoding="ISO-8859-1")
 
 col1, col2 = st.columns((2))
